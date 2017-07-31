db0d699ef0e0d551c3cd7e548a6c2429c9263ee6
leejaemin <jaeminstar@gmail.com>
2017-07-31 19:47:49 +0900

    initial commit

---
+++ src/main/java/com/codesquad/JwpCodesquadApplication.java
@@ -0,0 +1,12 @@
+package com.codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpCodesquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpCodesquadApplication.class, args);
+	}
+}
